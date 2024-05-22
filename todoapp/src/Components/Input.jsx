4b38510a5dc15390a4549da307f23082ad529c89
SANAL-C-T <sanal.palath@gmail.com>
2024-05-22 10:15:34 +0530

    corrected style

--- todoapp/src/Components/Input.jsx
+++ todoapp/src/Components/Input.jsx
@@ -1,15 +1,42 @@
+import { useState } from 'react';
 import './input.css'
-let Addform=()=>{
-return (
-    <form>
-        <div id='inputContainer'>
-        <input type="text" placeholder=" Add a task" name="task" id="taskInput"></input>
-        <button type='submit'>ADD TASK</button>
-        </div>
-       
 
-    </form>
-)
+
+
+let Addform=()=>{
+const [task,setTask]=useState("")
+
+const [listTask,setList]=useState([])
+
+const lockDefault=(e)=>{
+    e.preventDefault();
+    setList([...listTask,task]);
+    setTask("");
+  
+}
+
+        return (
+            <div id='inputContainer'>
+            <form onSubmit={lockDefault}>
+                <input type="text" placeholder=" Add a task" onChange={(e)=>setTask(e.target.value)} value={task} name="task" id="taskInput"></input>
+                <button type='submit'>ADD TASK</button>
+            </form>
+                    { listTask.map((x)=>{
+                        return (
+                            <div id='onetsk'>
+                                <h2>{x}</h2>
+                                <div id='controls'>
+                                <span>EDIT</span>
+                                <span>DELETE</span>
+                                <span>COMPLETED</span>
+                                </div>
+                              
+                                </div>
+                           
+                        )   
+                    }) }
+            </div>
+        )
 }
 
 export default Addform;
